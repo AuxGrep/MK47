bef7b6a03449e51a219a0e3ef8ee5aa188c76a0c
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2023-01-02 15:33:01 +0300

    Update mk47-wifi-SNIFFER.py

--- mk47-wifi-SNIFFER.py
+++ mk47-wifi-SNIFFER.py
@@ -8,6 +8,19 @@ import urllib.request
 import subprocess
 import sys
 
+colorcode = {
+
+	'RED':'\033[0;31m',
+	'RESET':'\033[0m',
+	'YELLOW':'\033[1;33m',
+	'ITALIC':'\033[3m',
+	'BLINK':'\033[5m'
+}
+
+if not os.geteuid() == 0:
+    sys.exit("\n{0}{2}PLease run this script as root{1}\n".format(colorcode['RED'], \
+		colorcode['RESET'], colorcode['ITALIC']))
+
 wifimonitor = str(sys.argv[1])
 
 # Now let's check if your pc is connected with internet
@@ -24,12 +37,14 @@ if network_manager():
 	os.system('sudo systemctl stop wpa_supplicant.service')
 	time.sleep(2)
 	os.system('clear')
-	print('Run Again this script')
+	print('{1}{0}Run Again this script{2}'.format(colorcode['ITALIC'], colorcode['YELLOW'], \
+		colorcode['RESET']))
 	sys.exit()
 else:
 
 # Then let's start sniffing for Wireless Network based on your card if it is 2.4ghz or both 2.4ghz and 5ghz 
-	print('Starting hcxdumptool process')
+	print('{0}{2}Starting hcxdumptool process{1}'.format(colorcode['YELLOW'], colorcode['RESET'], \
+		colorcode['ITALIC']))
 
 def hcxdump():
 	hcxdump_file = 'recon_dumped.pcapng'
@@ -37,9 +52,17 @@ def hcxdump():
 	subprocess.call(f'sudo hcxdumptool -i {wifimonitor} -o {hcxdump_file} --active_beacon --enable_status={stx}', shell=True)
 	subprocess.run("clear")
 	print(f'All wifi data saved as {hcxdump_file}, Highly recommend to use GPU to crack captured Networks')
-	print(f"Now run python3 hcx.py to convert dumped data from {hcxdump_file}")
+	print("Now run {1}{2}{4}sudo python3 hcx.py{3} to convert dumped data from {1}{2}{0}{3}".format(hcxdump_file, \
+		colorcode['RED'], colorcode['ITALIC'], \
+			colorcode['RESET'], colorcode['BLINK']))
 
 hcxdump();
+
+
+
+
+
+
 
 
 
